78e8c6aa6cbb7097560f52e28ff622c3e54cb78f
Carusovich <carlos.mascheroni89@gmail.com>
2022-04-16 23:21:00 -0300

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,16 @@ const NavBar = () => {
 <div className="navbar bg-base-100">
   <div className="flex-1">
     <a className="btn btn-ghost normal-case text-xl">Football Shop</a>
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </label>
+      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Camisetas</a></li>
+        <li><a>Calzado</a></li>
+        <li><a>Accesorios</a></li>
+      </ul>
+    </div>
   </div>
   <div className="flex-none">
     <div className="dropdown dropdown-end">
